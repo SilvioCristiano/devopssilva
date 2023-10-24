d5248eb73e76512683fefde0bcd8f5eec70ec6b8
“SilvioCristiano” <sivuca1@gmail.com>
2023-10-24 02:05:03 +0100

    teste

---
+++ devopssilva/src/main/java/com/devopssilva/DevopssilvaApplication.java
@@ -0,0 +1,13 @@
+package com.devopssilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopssilvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopssilvaApplication.class, args);
+	}
+
+}
---
+++ devopssilva/src/test/java/com/devopssilva/DevopssilvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopssilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopssilvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
